776829af44744e214ac3435b4de0078d83bd6994
Platon Matšinski <platonmatsinski@Platons-MacBook-Air.local>
2025-03-29 17:04:49 +0200

    Colors of the results changed from default colores

---
+++ Converters/ColorNameToBrushConverter.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Net.Mime;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+
+namespace Converters
+{
+    public class ColorNameToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is string colorName)
+            {
+                switch (colorName)
+                {
+                    case "Red":
+                        return Application.Current.FindResource("ResultRedBrush");
+                    case "Black":
+                        return Application.Current.FindResource("ResultBlackBrush");
+                    case "Green":
+                        return Application.Current.FindResource("ResultGreenBrush");
+                    default:
+                        return Brushes.Gray; // vaikevärv, kui midagi muud ei sobi
+                }
+            }
+            
+            return Brushes.Gray;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
